9cb37956a329ab17a67769dfa12f6525894a9423
jeffresto <112721088+jeffresto@users.noreply.github.com>
2024-10-22 09:18:41 -0400

    Create EnglishRuler.h

---
+++ English Ruler/EnglishRuler.h
@@ -0,0 +1,28 @@
+#ifndef ENGLISHRULER_H_
+#define ENGLISHRULER_H_
+
+#include <iostream>
+using namespace std;
+
+class EnglishRuler {
+public:
+	EnglishRuler(int nInches, int majorLength);
+	virtual ~EnglishRuler();
+
+	void drawRuler();
+
+	int getMajorLength() const;
+	void setMajorLength(int);
+	int getNInches() const;
+	void setNInches(int);
+
+private:
+	void drawOneTick(int tickLength, int tickLabel = -1);
+	void drawTicks(int tickLength);
+private:
+	int nInches;
+	int majorLength;
+
+};
+
+#endif
